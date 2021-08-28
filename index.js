2f0167817dd2676113453ecc879bd3d38e105991
adharuqudni <annas.adharuqudni@gmail.com>
2021-08-28 16:49:48 +0700

    first init

---
+++ index.js
@@ -0,0 +1,31 @@
+const judul = document.getElementById("judul");
+
+const ArrayAngka = [1,3,5,2,4,6,7,8,9,10];
+const angka2 = [1,12];
+
+// const gabungan = ArrayAngka.filter((satuanAngka)=>{
+//     if(satuanAngka % 2 === 0){
+//         console.log("angka ini genap",satuanAngka);
+//     }
+//     return satuanAngka % 2 !== 0;
+// });
+
+// const gabungan = ArrayAngka.findIndex((satuanAngka)=>{
+//     return satuanAngka === 18
+// });
+
+// const gabungan = ArrayAngka.map((satuanAngka)=>{
+//     return satuanAngka + 2
+// });
+
+// const gabungan = ArrayAngka.pop(); // ngeluarin dari array index terakhir
+// const gabungan = ArrayAngka.push(); // masukin nilai baru di index terakhir
+// const gabungan = ArrayAngka.shift();// ngeluarin dari array index pertama
+// const gabungan = ArrayAngka.unshift(12); // masukin nilai  dari array index pertama
+// const gabungan = ArrayAngka.reverse();
+// const gabungan = ArrayAngka.slice(3,8)// motong array
+// const gabungan = ArrayAngka.splice(5,3);// menghapus nilai array dari index ke x sebanyak z
+// const gabungan = ArrayAngka.toString();
+
+
+console.log(gabungan,ArrayAngka);
